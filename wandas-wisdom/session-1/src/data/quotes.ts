988403d66ae11f73fab59f1781588727b3cd8369
dkobrin <shdwkat999@yahoo.com>
2023-05-27 22:21:27 -0400

    implementing testing with jest. coverage for now is utilities folder.

--- wandas-wisdom/session-1/src/data/quotes.ts
+++ wandas-wisdom/session-1/src/data/quotes.ts
@@ -4,6 +4,19 @@ export type Quote = {
     tags: string[]
 }
 
+export const mock_quotes: Quote[] = [
+    {
+        text: `test text 1`,
+        author: 'auth1',
+        tags: ['one', 'two', 'three', 'four']
+    },
+    {
+        text: 'test text 2',
+        author: 'auth2',
+        tags: ['four', 'three', 'two', 'one']
+    },
+] 
+
 export const quotes: Quote[] = [
     {
         text: `Hope springs eternal in the human breast:\nMan never is, but always to be blest.`,
